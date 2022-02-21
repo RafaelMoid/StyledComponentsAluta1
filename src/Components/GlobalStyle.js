09d223a03808d59fe89d9d7b0ea47705f2fa5b7a
Rafael Varela <rafaelvrcr@gmail.com>
2022-02-21 09:31:49 -0300

    Config global styled and aply at app.js

--- src/Components/GlobalStyle.js
+++ src/Components/GlobalStyle.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
 * {
   box-sizing: border-box;
   font-family: "Montserrat", sans-serif;
@@ -9,4 +9,4 @@ const GlobalStyle = createGlobalStyle`
   text-decoration: none;
   color: grey;
 }
-`
+`;
