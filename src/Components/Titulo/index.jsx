7da6361a13cd9b791a5833e39941132fe98a16b6
Rafael Varela <rafaelvrcr@gmail.com>
2022-02-21 09:25:38 -0300

    master

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+
 import styled from 'styled-components';
 
 const Titulo = styled.h1`
@@ -6,8 +6,5 @@ const Titulo = styled.h1`
   padding: 25px 0;
 `
 
-// const Titulo = ({ children }) => {
-//   return <h1 className="titulo">{children}</h1>;
-// };
 
 export default Titulo;
